b7b0221ebc0782ba2a781330658e79181e3aa008
Reid Blomquist <reid.blomquist@gmail.com>
2016-04-23 20:24:56 -0700

    moving some stuff into a more sane spot; namespacing some work; have our new UI Fade/Slide working

--- Assets/Scripts/IObj/IObjManager.cs
+++ Assets/Scripts/IObj/IObjManager.cs
@@ -12,10 +12,8 @@ namespace Shounds.IObj
     {
         [SerializeField] private Reticle m_Reticle;                         // The scene only uses SelectionSliders so the reticle should be shown.
         [SerializeField] private SelectionRadial m_Radial;                  // Likewise, since only SelectionSliders are used, the radial should be hidden.
-        [SerializeField] private UIFader m_HowToUseFader;                   // This fader controls the UI showing how to use SelectionSliders.
-        [SerializeField] private SelectionSlider m_HowToUseSlider;          // This is the slider that is used to demonstrate how to use them.
-        [SerializeField] private UIFader m_HowToUseConfirmFader;            // Afterwards users are asked to confirm how to use sliders in this UI.
-        [SerializeField] private SelectionSlider m_HowToUseConfirmSlider;   // They demonstrate this using this slider.
+        [SerializeField] private IObjUIFader m_IObjMoveModeUIFader;                   // This fader controls the UI showing how to use SelectionSliders.
+        [SerializeField] private SelectionSlider m_IObjMoveModeUISlider;          // This is the slider that is used to demonstrate how to use them.
         [SerializeField] private UIFader m_ReturnFader;                     // The final instructions are controlled using this fader.
 
 
@@ -26,14 +24,14 @@ namespace Shounds.IObj
             m_Radial.Hide ();
 
             // In order, fade in the UI on how to use sliders, wait for the slider to be filled then fade out the UI.
-            yield return StartCoroutine (m_HowToUseFader.InteruptAndFadeIn ());
-            yield return StartCoroutine (m_HowToUseSlider.WaitForBarToFill ());
-            yield return StartCoroutine (m_HowToUseFader.InteruptAndFadeOut ());
+            yield return StartCoroutine (m_IObjMoveModeUIFader.InteruptAndFadeIn ());
+            yield return StartCoroutine (m_IObjMoveModeUISlider.WaitForBarToFill ());
+            yield return StartCoroutine (m_IObjMoveModeUIFader.InteruptAndFadeOut ());
 
             // In order, fade in the UI on confirming the use of sliders, wait for the slider to be filled, then fade out the UI.
-            yield return StartCoroutine(m_HowToUseConfirmFader.InteruptAndFadeIn());
-            yield return StartCoroutine(m_HowToUseConfirmSlider.WaitForBarToFill());
-            yield return StartCoroutine(m_HowToUseConfirmFader.InteruptAndFadeOut());
+            //yield return StartCoroutine(m_HowToUseConfirmFader.InteruptAndFadeIn());
+            //yield return StartCoroutine(m_HowToUseConfirmSlider.WaitForBarToFill());
+            //yield return StartCoroutine(m_HowToUseConfirmFader.InteruptAndFadeOut());
 
             // Fade in the final UI.
             yield return StartCoroutine (m_ReturnFader.InteruptAndFadeIn ());
